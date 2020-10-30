1fe3c71f276b5b3789a5358237dca84e9b7e2b72
Alan PINDER <apinder@adista.fr>
2020-10-30 17:39:37 +0100

    Initial commit

---
+++ PhotoSandbox/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using ImageMagick;
+
+namespace PhotoSandbox
+{
+    public partial class MainWindow : Window
+    {
+        public MagickImage magickImage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.magickImage = new MagickImage("Image.jpg");
+
+            Refresh();
+        }
+
+        public void Refresh()
+        {
+            using (Stream stream = new MemoryStream())
+            {
+                magickImage.Write(stream);
+                stream.Position = 0;
+
+                BitmapImage bitmapImage = new BitmapImage();
+
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = stream;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad; // Force chargement depuis stream
+                bitmapImage.EndInit();
+
+                this.imgImage.Source = bitmapImage;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# PHOTO SANDBOX
+
+Petit bac-a-sable pour tester l'integration de bibliothèque ImageMagick.
+
+Les objectifs:
+
+- Permettre l'utilisateur de rotater l'image 90/180/270/360
+- Permettre l'utilisateur de sauvegarder l'image sur le disque
+- Permettre l'utilisateur d'ouvrir une image existante
+- Permettre l'utilisateur de cropper l'image (facultatif)
+
+Soit par des boutons / menus ou autre.
